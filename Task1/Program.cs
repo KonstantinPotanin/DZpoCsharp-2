91510a96e86f7fe358c94e70b35675cdaea133f5
Konstantin <pkv90@yandex.ru>
2023-02-01 11:39:00 +0500

    Решение первой задачи

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int TwoDigit(int num)
+{
+    if (num > 99 && num < 1000)
+    {
+        num = num / 10;
+        num = num % 10;
+        return num;
+    }
+    else return -1;
+}   
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int newNum = TwoDigit(number);
+
+Console.WriteLine($"Ввели число {number}, получили новое число {newNum}");
